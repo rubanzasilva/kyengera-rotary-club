efe42196d5597baa1245598724006af93d04e13b
rubanzasilva <rubanza@rubanzasilver.xyz>
2023-08-23 15:52:36 +0300

    adding hero section

--- src/components/heroSection/data.js
+++ src/components/heroSection/data.js
@@ -1,16 +1,6 @@
-//data for about zicofe section
-export const SectionAinfo = {
-    id:'SelfIntro',
-    TitleText:'Hi, I am Rubanza Silver',
-    headline:'A Data Scientist with a background in Software Enginnering',
-    text:'My work and interest lies in working on various steps of the machine learning lifecycle from EDA, Data wrangling, Feature Engineering to Model building , deployment , testing, monitoring etc',
-    text1:'I also have vast experience building web applications with javascript, reactjs etc.',
-    text2:'Below are examples of my work',
-    imgStart:false,
-    // img:'https://d322abd807wvow.cloudfront.net/suwik-photos/eatGreen.jpeg',
-    img:'https://d3q0oijmiduzg2.cloudfront.net/rubanzasilver/silverPotraitCompressed.webp',
-    alt:'Picture of Rubanza Silver',
+export const homeObjB = {
+    title:'IMPROVING LIVES',
+    headline:'By Empowering People',
+    buttonText:'Donate Now',
+    video:'https://d2kmvpaplrh563.cloudfront.net/zicofe-photos/video.mp4'
 };
-
-
-
